8cfd8b2084b3d37349f267b23bebbb6097a5131d
Tarun Ramakrishna Elankath <tarun.ramakrishna.elankath@sap.com>
2017-11-01 07:15:57 +0530

    --wip-- [skip ci]

--- zshcfg/autorun/zplugs.zsh
+++ zshcfg/autorun/zplugs.zsh
@@ -1,18 +1,18 @@
 [ -f ~/.zplug/init.zsh ] && source ~/.zplug/init.zsh || return
 
-# For non Cygwin platforms load the pure prompt and its deps
+# Fancy stuff don't work in Cygwin
 if [[ -z $isCygwin ]]; then
   zplug mafredri/zsh-async, from:github
   zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
   zplug supercrabtree/k
+  zplug changyuheng/zsh-interactive-cd
+  zplug RobSis/zsh-completion-generator
 fi
 zplug modules/utility, from:prezto
 zplug zsh-users/zsh-syntax-highlighting, defer:2
 zplug plugins/git, from:oh-my-zsh 
-zplug changyuheng/zsh-interactive-cd
 zplug docker/cli, use:contrib/completion/zsh
 zplug docker/compose, use:contrib/completion/zsh
-zplug RobSis/zsh-completion-generator
 [ $isMacos ] && zplug modules/homebrew, from:prezto
 
 
