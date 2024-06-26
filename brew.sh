03afcefdbc1aeccc765cdc75826840819e9e92b3
madhav bhargava <madhav.bhargava@sap.com>
2024-06-26 11:43:53 +0530

    created a dedicated script for brew and macos-defaults

---
+++ brew.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+# install command line tools using https://brew.sh/
+
+# Update to the latest version of homebrew
+# -----------------------------------------
+brew update
+# Upgrade already installed formulae.
+# -----------------------------------------
+brew upgrade
+
+BREW_INSTALL_PATH=$(brew --prefix)
+
+# GNU command line utilities
+# -----------------------------------------
+brew install coreutils \
+parallel \
+iproute2mac \
+gnu-sed \
+gnu-tar \
+grep \
+gzip
+
+# search and parsing utilities
+# -----------------------------------------
+brew install fd \
+jq \
+yq \
+ctags \
+the_silver_searcher \
+ripgrep
+
+# fortune-cookie-generator for some fun
+# -----------------------------------------
+brew install fortune \
+cowsay
+
+# language runtimes
+# -----------------------------------------
+brew install node \
+go \
+lua \
+deno
+
+# package managers
+# -----------------------------------------
+brew install yarn \
+luarocks
+
+# k8s command line utilities
+# -----------------------------------------
+brew install kubectl \
+krew \
+kube-ps1 \
+k9s
+
+# tail k8s logs with kubetail
+# -----------------------------------------
+brew tap johanhaleby/kubetail && brew install kubetail
+
+# gardener utilities
+# -----------------------------------------
+brew install gardener/tap/gardenctl-v2 \
+int128/kubelogin/kubelogin \
+gardener/tap/gardenlogin 
+
+# miscellaneous utilities
+# -----------------------------------------
+brew install rlwrap \
+openvpn \
+tree-sitter \ # parser generator tool
+bottom \ # https://github.com/ClementTsang/bottom
+rectangle \
+watch \
+cmake
+
+# editors
+# -----------------------------------------
+brew install neovim
+brew install --cask visual-studio-code
+
+# pdf reader
+# -----------------------------------------
+brew install skim 
+
+# git utilities
+# -----------------------------------------
+brew install lazygit \
+gh
+
+# fonts
+# ------------------------------------------
+
+fonts_list=(
+  font-3270-nerd-font
+  font-fira-mono-nerd-font
+  font-inconsolata-go-nerd-font
+  font-inconsolata-lgc-nerd-font
+  font-inconsolata-nerd-font
+  font-monofur-nerd-font
+  font-overpass-nerd-font
+  font-ubuntu-mono-nerd-font
+  font-agave-nerd-font
+  font-arimo-nerd-font
+  font-anonymice-nerd-font
+  font-aurulent-sans-mono-nerd-font
+  font-bigblue-terminal-nerd-font
+  font-bitstream-vera-sans-mono-nerd-font
+  font-blex-mono-nerd-font
+  font-caskaydia-cove-nerd-font
+  font-code-new-roman-nerd-font
+  font-cousine-nerd-font
+  font-daddy-time-mono-nerd-font
+  font-dejavu-sans-mono-nerd-font
+  font-droid-sans-mono-nerd-font
+  font-fantasque-sans-mono-nerd-font
+  font-fira-code-nerd-font
+  font-go-mono-nerd-font
+  font-gohufont-nerd-font
+  font-hack-nerd-font
+  font-hasklug-nerd-font
+  font-heavy-data-nerd-font
+  font-hurmit-nerd-font
+  font-im-writing-nerd-font
+  font-iosevka-nerd-font
+  font-jetbrains-mono-nerd-font
+  font-lekton-nerd-font
+  font-liberation-nerd-font
+  font-meslo-lg-nerd-font
+  font-monoid-nerd-font
+  font-mononoki-nerd-font
+  font-mplus-nerd-font
+  font-noto-nerd-font
+  font-open-dyslexic-nerd-font
+  font-profont-nerd-font
+  font-proggy-clean-tt-nerd-font
+  font-roboto-mono-nerd-font
+  font-sauce-code-pro-nerd-font
+  font-shure-tech-mono-nerd-font
+  font-space-mono-nerd-font
+  font-terminess-ttf-nerd-font
+  font-tinos-nerd-font
+  font-ubuntu-nerd-font
+  font-victor-mono-nerd-font
+)
+
+for font in "${fonts_list[@]}"
+do
+  brew install --cask "$font"
+done
+
+# remove outdated versions
+brew cleanup
---
+++ macos-defaults.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Keyboard setup
+# -----------------------------
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
+
+# Trackpad, mouse setup and bluetooth
+# ------------------------------------
+
+# Trackpad: swipe between pages with three fingers
+defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerHorizSwipeGesture -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
+
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# Screen
+# ------------------------------------
+
+# Save screenshots to ~/Screenshots folder
+SCREENSHOT_FOLDER="${HOME}/Screenshots"
+mkdir -p "${SCREENSHOT_FOLDER}"
+defaults write com.apple.screencapture location -string "${SCREENSHOTS_FOLDER}"
+
+# Dock
+# ------------------------------------
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# No bouncing icons
+defaults write com.apple.dock no-bouncing -bool true
+
+# Don't show recently used applications in the Dock
+defaults write com.Apple.Dock show-recents -bool false
+
+# Activity Monitor
+# ----------------------------------
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
